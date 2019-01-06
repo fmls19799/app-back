908b3d14759b25c70c6d82f1988304c9f424cf4d
fran <fmls19799@gmail.com>
2019-01-06 15:28:09 +0100

    added mlab connection and included gitignore

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
-// const DB_NAME = "back-end";
-const MONGO_URI = process.env.MONGO_URI;
+const MONGO_URI_START= process.env.MONGO_URI_START;
+const MONGO_URI_END= process.env.MONGO_URI_END;
 
-mongoose.connect(MONGO_URI, { useNewUrlParser: true })
-.then(()=>console.log(`Connected to db ${MONGO_URI}`))
+// USER CHOSEN TO CONNECT TO MLAB => 
+const USER=  process.env.USER_MLAB_FRAN;
+const USER_MLAB_PASSWORD= process.env.USER_MLAB_PASSWORD;
+
+mongoose.connect(`${MONGO_URI_START}${USER}:${USER_MLAB_PASSWORD}${MONGO_URI_END}`, { useNewUrlParser: true })
+.then((data)=>console.log(`Connected to db MLAB - ${data.connection.name}`))
 .catch(error => console.log(`An error ocurred ${error}`));
